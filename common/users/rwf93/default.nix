534c2ede65abd540497f6edbb2a4839e9065e1ba
rwf93 <22036195+rwf93@users.noreply.github.com>
2024-05-04 15:38:00 -0700

    dialout

--- common/users/rwf93/default.nix
+++ common/users/rwf93/default.nix
@@ -15,6 +15,7 @@
       "docker"
       "video"
       "render"
+      "dialout"
     ];
     openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJO4bJlRW7X9bytFCVWD/TweFqxgSHA2YcDISLpTSXI1FcXWZjZKKYhb/rEyjXneaxl4TcoW381Nc0pphzTz4VNe13GqZ0mF7r0NDnKA94XZcDKd3djDzBbslMdtVtxIgxVovbMysMndwHqZL6Qsp/m+eo0DKj/8mDT0vokiriyLL5M4ldkXxGnUjjNZ6QsLLCRw0EsuXO1TC2ZSsB4TjeM/vQPHcGngBWBBzzHKY0E/tnwxs32/bu7pVe38Q5UIWeFFar0xN/R6ctjfMIuIVp34uP51cL5hQ94O7U0YRPoJ+T+1544wJT3+GhX4ykEKxDl7SI0ZpMcPqPgnNE/8fcO6tMRnfUKGEMACagVrnsp9Kq71Mqp2Qy1ksXzV5pYdPcC+UtrLQULPVoVszBBIBA9XtLkB9WK9ZIph+pUCdQndIuDIUgkvr4geCoT0L6qktuPA+GuiPbxq5w05E5AR3WCrkRepkzyDTtBTAMPCP/X6BbUPrngIfxDql7KisN4RSEWDnlOGud8y2G8kCQxgI2K/QtMVOyyYVJskDaNQ0Tg4DH4wmL3Xq3JR7vJURFcY7Ml31Foqwfd3wfgIzeNsm3Vc+IEN7sC5YeEWfO/s8tKFSJWnkHng0FKdtE95CnhRNXL82gIsW2vGvtOcXpSdhjMe1ka/vYqmKxFq6b/qA9oQ=="];
   };
--- common/users/rwf93/home.nix
+++ common/users/rwf93/home.nix
@@ -1,15 +1,15 @@
 {
   pkgs,
-  home-manager,
-  ...
+    home-manager,
+    ...
 }: {
   home-manager.users.rwf93 = { 
     home.stateVersion = "23.11";
     programs = {
       git = {
         enable = true;
-	userName = "rwf93";
-	userEmail = "22036195+rwf93@users.noreply.github.com";
+        userName = "rwf93";
+        userEmail = "22036195+rwf93@users.noreply.github.com";
       };
     };
   };
