6db78e3892b7ea7a048e418660c37a13f192f37e
rwf93 <22036195+rwf93@users.noreply.github.com>
2024-05-16 15:58:30 -0700

    removed pass_secret_service

--- common/default.nix
+++ common/default.nix
@@ -8,5 +8,4 @@
   networking.hostName = deviceConfig.hostName;
   services.automatic-timezoned.enable = true;
   services.gnome.gnome-keyring.enable = true;
-  services.passSecretService.enable = true;
 }
